4e658f985876135202b4f744b22544c2e6cf3850
yuan <2570837397@qq.com>
2018-10-15 15:31:44 +0800

    使用springboot整合Thymeleaf

--- bool-hello/src/main/java/com/yuan/boot/controller/HelloController.java
+++ bool-hello/src/main/java/com/yuan/boot/controller/HelloController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +22,15 @@ public class HelloController {
 
 //    private Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @ApiOperation(value = "接收hello请求",notes = "接收hello请求") //使用该注解表示在使用swagger生成api文档时对该方法的说明
+    @ApiOperation(value = "跳转到index页面",notes = "跳转到index页面") //使用该注解表示在使用swagger生成api文档时对该方法的说明
     @ApiImplicitParam(value = "用户的用户名",name = "name" ,required = true,dataType ="String") //使用该注解表示对该方法上的参数的说明
     @GetMapping("/index/{name}")
-    public String hello(@PathVariable(name = "name") String name){
+    public String hello(@PathVariable(name = "name") String name, Model model){
 //        logger.trace("成功处理hello请求.........................");
         System.out.println("成功处理hello请求.........................");
         LoggerUtil.getLogger(this.getClass()).trace("成功处理hello请求.........................");
-         return "hello this my test springboot project!!!";
+        model.addAttribute("mesage","欢迎用户"+name+"!!!");
+         return "index";
     }
 
 
